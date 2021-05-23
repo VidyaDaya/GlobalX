b85c317e85369ffb159d5e1a19dc26e8c4954d60
VidyaDaya <vdd318@gmail.com>
2021-05-23 12:15:35 +1000

    Restructured code -Single responsibility

--- Program.cs
+++ Program.cs
@@ -1,33 +1,85 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using TextFileHelper;
+using System.Collections.Generic;
 
 namespace GlobalX
-{
-    class Program
+{ 
+    public class Program
     {
         static void Main(string[] args)
         {
-            TextFileReader fileObject=new TextFileReader();
-
-            fileObject.ReadFile("unsorted-names-list.txt");
+            NameSorter nameSorter= new NameSorter();
+            Console.WriteLine("Enter File Name: ");
+            nameSorter.FileName=Console.ReadLine();
+            nameSorter.NameSorterProcessor();
         }
     }
 
+    public interface INameSorter
+    {
+        public void ReadFile();
+        public void WriteFile();
+    }
+
+    class NameSorter : INameSorter
+    {
+        public string FileName { get; set;}
+        private List<PersonDetail> _nameList{get; set;}
+        
+
+        public NameSorter()
+        {
+            _nameList=new List<PersonDetail>();
+        }
+
+
+        public void NameSorterProcessor()
+        {
+            ReadFile();
+            SortNames();
+            WriteFile();
+        }
+
+        public void ReadFile()
+        {
+            if (File.Exists(FileName)) {     
+            using(StreamReader file = new StreamReader(FileName)) {
+            string line; 
+            while ((line = file.ReadLine()) != null) {
+                   var lastName=line.Split(' ').LastOrDefault();
+                    var givenName=line.Substring(0, line.LastIndexOf(' ')).TrimEnd(); 
+                    _nameList.Add(new PersonDetail{GivenName=givenName,LastName=lastName}); 
+            }  
+                file.Close();
+            }
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+
+        public void SortNames()
+        {
+            _nameList=_nameList.OrderBy(x=>x.LastName).ThenBy(x=>x.GivenName).ToList();
+        }
+
+        public void WriteFile()
+        {
+            using StreamWriter writeFile = new StreamWriter("sorted-names-list.txt");
+            foreach (PersonDetail person in _nameList)
+            {
+                var line=person.GivenName+ " " +person.LastName;
+                writeFile.WriteLine(line);
+            }
+            writeFile.Close();
+        }
+    }
 
     public class PersonDetail
     {
         public string GivenName{get; set;}
         public string LastName{get; set;}
-
-        
     }
-
-    
-
-    
-
-    
-
 }
--- TextFileHelper.cs
+++ TextFileHelper.cs
@@ -1,50 +1,57 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using GlobalX;
+// using System;
+// using System.IO;
+// using System.Linq;
+// using System.Collections.Generic;
+// using GlobalX;
 
-namespace TextFileHelper{
+// namespace TextFileHelper{
 
-    public  class TextFileReader
-    {
-        //  string textFile="unsorted-names-list.txt";
-
-        List<PersonDetail> nameList=new List<PersonDetail>();
-        PersonDetail name= new PersonDetail();
+//     public  class TextFileManager
+//     {
+//         List<PersonDetail> nameList=new List<PersonDetail>();
+//         PersonDetail name= new PersonDetail();
         
-        public void ReadFile(string fileName) {
+//         public List<PersonDetail> ReadTextFile(string fileName) {
             
-            if (File.Exists(fileName)) {     
-            using(StreamReader file = new StreamReader(fileName)) {
-                string ln; 
-                // List<string> lNames=new List<string>(); 
-            
-                while ((ln = file.ReadLine()) != null) {
-                   name.LastName=ln.Split(' ').LastOrDefault();
-                    name.GivenName=ln.Substring(0, ln.LastIndexOf(' ')).TrimEnd(); 
-                    nameList.Add(new PersonDetail{GivenName=name.GivenName,LastName=name.LastName}); 
-            }  
-                file.Close();  
-                nameList=nameList.OrderBy(x=>x.LastName).ThenBy(x=>x.GivenName).ToList();
-        foreach (PersonDetail person in nameList)
-        {
-            Console.WriteLine(person.LastName);
-        }
+//             if (File.Exists(fileName)) {     
+//             using(StreamReader file = new StreamReader(fileName)) {
+//                 string ln; 
+//             while ((ln = file.ReadLine()) != null) {
+//                    name.LastName=ln.Split(' ').LastOrDefault();
+//                     name.GivenName=ln.Substring(0, ln.LastIndexOf(' ')).TrimEnd(); 
+//                     nameList.Add(new PersonDetail{GivenName=name.GivenName,LastName=name.LastName}); 
+//             }  
+//                 file.Close();
+//                 return nameList;  
+//             }
+//             }
+//             else
+//             {
+//                 Console.WriteLine("File not found");
+//                 return null;
+//             }
+//         }
 
-        using StreamWriter newFile = new StreamWriter("sorted-names-list.txt");
+//         public void WriteTextFile(List<PersonDetail> nameList)
+//         {
+        
+//         string ln;
+//         nameList=nameList.OrderBy(x=>x.LastName).ThenBy(x=>x.GivenName).ToList();
+        
+//         foreach (PersonDetail person in nameList)
+//         {
+//             Console.WriteLine(person.LastName);
+//         }
 
-        foreach (PersonDetail person in nameList)
-        {
-           ln=person.GivenName+ " " +person.LastName;
-           newFile.WriteLine(ln);
-        }
-        newFile.Close();
-        }
-        }
-        else{
-            Console.WriteLine("The File {fileName} does not exist.");
-        }  
-        }
-}
-}
+//         using StreamWriter newFile = new StreamWriter("sorted-names-list.txt");
+
+//         foreach (PersonDetail person in nameList)
+//         {
+//            ln=person.GivenName+ " " +person.LastName;
+//            newFile.WriteLine(ln);
+//         }
+//         newFile.Close();
+        
+//         }
+//         }
+// }
